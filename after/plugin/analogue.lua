e94407d14597359bbb1679b6b34d1ee80b82a1dc
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2024-05-31 10:56:39 +0330

    comment out analogue

--- after/plugin/analogue.lua
+++ after/plugin/analogue.lua
@@ -1,1 +1,6 @@
-require("analogue").setup({})
+-- require("analogue").setup({
+-- 	auto_start = true,
+-- 	border = 'rounded', -- single, double, rounded
+-- 	fixed_position = "bottom-right",
+-- })
+
